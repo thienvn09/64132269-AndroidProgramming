c1dd720caab9e6dd3245bc7be3c3531a4dc638fd
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-02-25 23:38:59 +0700

    App đổi tiền

--- BaiTapLamThem_App_Doi_Don_vi_tien_te/app/src/main/java/thien/com/example/baitaplamthem_app_doi_don_vi_tien_te/MainActivity.java
+++ BaiTapLamThem_App_Doi_Don_vi_tien_te/app/src/main/java/thien/com/example/baitaplamthem_app_doi_don_vi_tien_te/MainActivity.java
@@ -1,7 +1,6 @@
-package thien.com.example.lab1;
+package thien.com.example.baitaplamthem_app_doi_don_vi_tien_te;
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,14 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        View mainView = findViewById(R.id.main);
-        if (mainView != null) {
-            ViewCompat.setOnApplyWindowInsetsListener(mainView, (v, insets) -> {
-                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-                return insets;
-            });
-        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
-
 }
---
+++ BaiTapLamThem_App_Doi_Don_vi_tien_te/app/src/test/java/thien/com/example/baitaplamthem_app_doi_don_vi_tien_te/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package thien.com.example.baitaplamthem_app_doi_don_vi_tien_te;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}

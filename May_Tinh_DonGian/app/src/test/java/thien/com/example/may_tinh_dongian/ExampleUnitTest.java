1eb6ce815337c4ad01dc93c9fdcd41902008e210
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-02-28 16:08:01 +0700

    upp

---
+++ May_Tinh_DonGian/app/src/test/java/thien/com/example/may_tinh_dongian/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package thien.com.example.may_tinh_dongian;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}

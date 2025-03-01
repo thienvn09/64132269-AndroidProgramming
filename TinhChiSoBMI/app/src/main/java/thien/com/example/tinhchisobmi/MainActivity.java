5f69d563ce7f2b076c14584bc44a105e4d84ad5f
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-03-01 13:16:01 +0700

    www

--- TinhChiSoBMI/app/src/main/java/thien/com/example/tinhchisobmi/MainActivity.java
+++ TinhChiSoBMI/app/src/main/java/thien/com/example/tinhchisobmi/MainActivity.java
@@ -1,7 +1,9 @@
 package thien.com.example.tinhchisobmi;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,18 +12,43 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText Txt_ChieuCao,Txt_CanNang;
-    private Button KQ;
-    
+    private EditText Txt_ChieuCao,txt_CanNang;
+    private Button Tinh;
+    private TextView KQ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Txt_ChieuCao = findViewById(R.id.Txt_ChieuCao);
+        txt_CanNang = findViewById(R.id.txt_CanNang);
+        Tinh = findViewById(R.id.Tinh);
+        KQ = findViewById(R.id.KQ);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public String getTxt_ChieuCao()
+    {
+        return Txt_ChieuCao.getText().toString();
+
+    }
+    public String gettxt_CanNang()
+    {
+        return txt_CanNang.getText().toString();
+    }
+    public void setKQ(String value )
+    {
+        KQ.setText(value);
+    }
+    public void Tinh1()
+    {
+        double CanNang = Double.parseDouble(gettxt_CanNang());
+        double ChieuCao = Double.parseDouble(gettxt_CanNang());
+        double DMI = (CanNang)/(ChieuCao*ChieuCao);
+        
+        setKQ(DMI);
     }
 }

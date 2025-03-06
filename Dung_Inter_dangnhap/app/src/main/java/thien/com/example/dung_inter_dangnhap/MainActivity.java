8954177be7901945e93963d5118031e5cc55b028
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-03-06 23:00:30 +0700

    test thử

--- Dung_Inter_dangnhap/app/src/main/java/thien/com/example/dung_inter_dangnhap/MainActivity.java
+++ Dung_Inter_dangnhap/app/src/main/java/thien/com/example/dung_inter_dangnhap/MainActivity.java
@@ -1,6 +1,8 @@
 package thien.com.example.dung_inter_dangnhap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -17,9 +19,10 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        Button btnLogin = (Button) findViewById(R.id.btnLogin);
-        // sự kiện
-        btnLogin.setOnClickListener(new View.setOnClickListener());
-
+    }
+    // su dung inter tuong minh
+    public void ChuyenSangTrangDN(View v){
+        Intent intent =new Intent(MainActivity.this,XuLylogin.class);
+        startActivity(intent);
     }
 }

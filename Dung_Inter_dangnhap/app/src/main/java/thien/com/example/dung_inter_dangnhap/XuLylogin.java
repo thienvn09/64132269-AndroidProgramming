9f1e6af7d7b8d2cd68058a836b11adb90346da36
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-03-07 00:20:37 +0700

    Update XuLylogin.java

--- Dung_Inter_dangnhap/app/src/main/java/thien/com/example/dung_inter_dangnhap/XuLylogin.java
+++ Dung_Inter_dangnhap/app/src/main/java/thien/com/example/dung_inter_dangnhap/XuLylogin.java
@@ -1,5 +1,6 @@
 package thien.com.example.dung_inter_dangnhap;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -40,7 +41,7 @@ public class XuLylogin extends AppCompatActivity {
                 mk ="1";
                 if(getInputTk() == tk && getInputMK() == mk )
                 {
-
+                    Intent intent = new Intent(XuLylogin.this,)
                 }
             }
         });

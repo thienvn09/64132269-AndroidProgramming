01a2d63258ef472bc8f602fbaf9d90d75dc872df
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-03-06 23:37:29 +0700

    tinh chỉnh một số thứ

--- Dung_Inter_dangnhap/app/src/main/java/thien/com/example/dung_inter_dangnhap/XuLylogin.java
+++ Dung_Inter_dangnhap/app/src/main/java/thien/com/example/dung_inter_dangnhap/XuLylogin.java
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class XuLylogin extends AppCompatActivity {
-
+    public
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

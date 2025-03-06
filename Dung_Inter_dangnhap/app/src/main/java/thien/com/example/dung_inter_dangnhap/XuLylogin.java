27d825ab0d4064c7084873b2f0aa0c63f5f033ea
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-03-07 00:11:32 +0700

    xử lý logic đăng nhập

--- Dung_Inter_dangnhap/app/src/main/java/thien/com/example/dung_inter_dangnhap/XuLylogin.java
+++ Dung_Inter_dangnhap/app/src/main/java/thien/com/example/dung_inter_dangnhap/XuLylogin.java
@@ -1,6 +1,9 @@
 package thien.com.example.dung_inter_dangnhap;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,32 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class XuLylogin extends AppCompatActivity {
+    private EditText inputTk,inputMK,etxtThongBao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_xu_lylogin);
+        inputTk = findViewById(R.id.inputTk);
+        inputMK =findViewById(R.id.inputMK);
+        etxtThongBao=findViewById(R.id.etxtThongBao);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+    public String getInputTk(){
+        return inputTk.getText().toString();
+    }
+    public String getInputMK()
+    {
+        return inputMK.getText().toString();
+    }
+    Button btnDN=findViewById(R.id.btnDN);
+    btnDN.setOnClickListener(new View.OnLongClickListener(){
+        @Override
+    })
 }

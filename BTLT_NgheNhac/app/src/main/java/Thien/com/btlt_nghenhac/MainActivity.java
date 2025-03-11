15071f1c3a2de2d0c5868b4ed8c405c7fda73cd0
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-03-11 12:43:37 +0700

    đăng nhập cơ bảng

--- BTLT_NgheNhac/app/src/main/java/Thien/com/btlt_nghenhac/MainActivity.java
+++ BTLT_NgheNhac/app/src/main/java/Thien/com/btlt_nghenhac/MainActivity.java
@@ -1,6 +1,8 @@
 package Thien.com.btlt_nghenhac;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -13,12 +15,33 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     private EditText txtInputMK,txtInputTk;
     private Button btnDN;
-    
+    public String getTxtInputMk(){
+        return txtInputMK.getText().toString();
+    }
+    public String getTxtInputTk()
+    {
+        return txtInputTk.getText().toString();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        // ánh xạ
+        txtInputTk = findViewById(R.id.txtInputTK);
+        txtInputMK = findViewById(R.id.txtInputMK);
+        btnDN = findViewById(R.id.btnDN);
+        btnDN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(getTxtInputTk().equals("thien") && getTxtInputMk().equals("1"))
+                {
+                    Intent intent = new Intent(MainActivity.this, TrangChu.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);

d913bc2491fab2f559b856e2e0f3d0c55b271bfe
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-03-11 12:33:31 +0700

    hoàng thành layout đăng nhập

--- BTLT_NgheNhac/app/src/main/java/Thien/com/btlt_nghenhac/MainActivity.java
+++ BTLT_NgheNhac/app/src/main/java/Thien/com/btlt_nghenhac/MainActivity.java
@@ -1,6 +1,8 @@
 package Thien.com.btlt_nghenhac;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText txtInputMK,txtInputTk;
+    private Button btnDN;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

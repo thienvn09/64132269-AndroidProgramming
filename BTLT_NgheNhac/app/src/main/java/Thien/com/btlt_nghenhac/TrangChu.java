c176ccb941305d6a884f94eeb16c3692f78a905c
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-03-12 11:52:31 +0700

    xử lý layout chơi nhạc

--- BTLT_NgheNhac/app/src/main/java/Thien/com/btlt_nghenhac/TrangChu.java
+++ BTLT_NgheNhac/app/src/main/java/Thien/com/btlt_nghenhac/TrangChu.java
@@ -3,6 +3,7 @@ package Thien.com.btlt_nghenhac;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -25,10 +26,11 @@ public class TrangChu extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_trang_chu);
 
+        // ánh xạ
         txtChaoMung = findViewById(R.id.txtChaoMung);
         listDsbaihat = findViewById(R.id.listDsbaihat);
         String tenDn =getIntent().getStringExtra("123");
-        txtChaoMung.setText(" " + tenDn + "chúc bạn một ngày tốt lành ");
+        txtChaoMung.setText( "chúc " + tenDn + " một ngày tốt lành ");
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -36,18 +38,27 @@ public class TrangChu extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         dsbaihat.add("Không Thể Say - Ca sĩ : Hiếu Monday");
         dsbaihat.add("Âm Thầm Bên Em- Ca sĩ : G-Dragon Việt Nam");
+
+        // dùng adapter để truyền dữ liệu vào listview
         ArrayAdapter<String> adapterlistDsbaihat = new ArrayAdapter<>(this, android.R.layout.simple_list_item_activated_1, dsbaihat);
         listDsbaihat.setAdapter(adapterlistDsbaihat);
 
-        listDsbaihat.setOnItemClickListener((parent, view, position, id)
-        Tham số Kiểu dữ liệu	Ý nghĩa
-        parent	AdapterView<?>	ListView chứa item được bấm (Chính là listDsbaihat)
-        view	View	Giao diện của item được bấm (Là TextView nếu dùng simple_list_item_1.xml)
-        position	int	Vị trí của item được bấm trong danh sách (dsbaihat.get(position))
-        id	long	ID của item, nhưng với ArrayAdapter<String>, nó thường giống position
-        listDsbaihat.setOnItemClickListener(new View)
+        // bắt sự kiện khi chạm vào màng hình
+        listDsbaihat.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String BaiHatDuocchon = dsbaihat.get(position);
+                String BaiHat1 = dsbaihat.get(0); // test
+                String BaiHat2 = dsbaihat.get(1); // test
+                // mở view mới
+                Intent intent = new Intent(TrangChu.this, PhatNhac.class);
+                intent.putExtra("1",BaiHatDuocchon);
+                startActivity(intent);
+            }
+        });
 
     }
 }

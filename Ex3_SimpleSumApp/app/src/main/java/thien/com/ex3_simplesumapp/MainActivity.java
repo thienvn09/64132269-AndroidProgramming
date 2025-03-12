f2916ee27dcd894fbe0c521ed1d5e0ce5b5814eb
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-03-12 23:23:25 +0700

    hoàng thành logic cho bài tính sum đơn giản

--- Ex3_SimpleSumApp/app/src/main/java/thien/com/ex3_simplesumapp/MainActivity.java
+++ Ex3_SimpleSumApp/app/src/main/java/thien/com/ex3_simplesumapp/MainActivity.java
@@ -1,6 +1,9 @@
 package thien.com.ex3_simplesumapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,40 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    public TextView inputA,inputB,OutPut;
+    public Button btnTinh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        inputA = findViewById(R.id.inputA);
+        inputB = findViewById(R.id.inputB);
+        OutPut = findViewById(R.id.OutPut);
+        btnTinh = findViewById(R.id.btnTinh);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public String getinputA()
+    {
+        return inputA.getText().toString();
+    }
+    public String getInputB()
+    {
+        return inputB.getText().toString();
+    }
+    public void setKetQua(String value) {
+        OutPut.setText(value);
+    }
+
+    public void XuLyCong(View view)
+    {
+        double a = Double.parseDouble(getinputA());
+        double b = Double.parseDouble(getInputB());
+        double KQ_Cong = a+b;
+        setKetQua("kết quả "+KQ_Cong);
     }
 }

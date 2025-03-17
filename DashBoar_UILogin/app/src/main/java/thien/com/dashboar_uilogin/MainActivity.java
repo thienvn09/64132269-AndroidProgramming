afd267e80e9998c179a851576f5f1e04469471ba
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-03-17 22:07:41 +0700

    xu ly logic va qua viewdashboard

--- DashBoar_UILogin/app/src/main/java/thien/com/dashboar_uilogin/MainActivity.java
+++ DashBoar_UILogin/app/src/main/java/thien/com/dashboar_uilogin/MainActivity.java
@@ -1,6 +1,10 @@
 package thien.com.dashboar_uilogin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +13,33 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    public EditText edtDN,edtPass;
+    public Button loginButton;
+    public String getedtDn()
+    {
+        return edtDN.getText().toString();
+    }
+    public String getedtPass()
+    {
+        return edtPass.getText().toString();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        edtDN = findViewById(R.id.edtDN);
+        edtPass = findViewById(R.id.edtPass);
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(getedtDn().equals("Thien") && getedtPass().equals("12")){
+                    Intent intent = new Intent(MainActivity.this,DashBoard.class);
+                    startActivity(intent);
+                }
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);

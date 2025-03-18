813c232b2904f644e821daf3ff63388dcfd8be1b
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-03-18 09:58:48 +0700

    thêm font chữ

--- Test/app/build.gradle.kts
+++ Test/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-
+    implementation ("androidx.emoji2:emoji2:1.2.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)

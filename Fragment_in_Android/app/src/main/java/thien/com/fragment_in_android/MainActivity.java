100ff0481abdc29c3b4bd21ae2c14e020b078249
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-04-18 09:23:22 +0700

    layout

--- Fragment_in_Android/app/src/main/java/thien/com/fragment_in_android/MainActivity.java
+++ Fragment_in_Android/app/src/main/java/thien/com/fragment_in_android/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +17,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.frameMain, new ContentFragment())
+                .add(R.id.Framefoot, new FooterFragment())
+                .commit();
+
     }
 }

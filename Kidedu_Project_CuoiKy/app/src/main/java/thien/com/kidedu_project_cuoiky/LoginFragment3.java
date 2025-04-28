c042b0c765db1eb85064f481b8c4090a97dfe95b
thienvn09 <117313593+thienvn09@users.noreply.github.com>
2025-04-28 20:11:15 +0700

    Làm giao diện đăng nhập
    Khá ưng ý <3

--- Kidedu_Project_CuoiKy/app/src/main/java/thien/com/kidedu_project_cuoiky/LoginFragment3.java
+++ Kidedu_Project_CuoiKy/app/src/main/java/thien/com/kidedu_project_cuoiky/LoginFragment3.java
@@ -2,7 +2,6 @@ package thien.com.kidedu_project_cuoiky;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
